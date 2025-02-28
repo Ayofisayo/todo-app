89a3e38995968e9e73d89bb356c90bd73b150936
oluwafisayo <oluwafisayotheophilus@gmail.com>
2025-02-28 06:38:41 -0500

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+#when you define the function, it is called the parameter of the function
+#there are some functions that get arguments
+#when you call the function the argument value
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
+
+#this will not allow the print statement to work when called
+#in main.py
+#if we run name which is variable directly in the same py file.
+# the output will be main. but if we run functions.py which
+# contain the __name__ in the main.py the output will be functions.
+# to control the execution when the file is run directly
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
---
+++ main.py
@@ -0,0 +1,72 @@
+from functions import get_todos, write_todos
+import time
+
+now = time.strftime("%b %d, %Y %H:%M:%S")
+print("It is", now)
+
+while True:
+    # Get user input and strip space chars from it
+    user_action = input("Type add, exit, edit, complete or show: ")
+    user_action = user_action.strip()
+
+    # Check if user action is 'add'
+    if user_action.startswith('add'):
+        todo= user_action[4:] #a break line for each item in the textfile
+
+        todos = get_todos()
+
+        todos.append(todo + '\n')
+        # Write todos in the file
+        write_todos( todos, "todos.txt")
+
+    elif user_action.startswith('show'):
+
+        todos = get_todos()
+
+        # new_todos = [item.strip('\n') for item in todos]
+
+        for index, item in enumerate(todos):
+            item = item.strip('\n')
+            item = item.title()
+            row = f"{index + 1}-{item}"
+            print(row)
+
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+            print(number)
+            number = number - 1
+
+            todos = get_todos()
+
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo + '\n'
+
+            write_todos(todos)
+        except ValueError:
+            print("Your command is not valid.")
+            continue
+
+    elif user_action.startswith('complete'): # to remove item from the list
+        try:
+            number = int(user_action[9:])
+
+            todos = get_todos()
+
+            index = number -1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+
+            write_todos(todos)
+
+            message = f"Todo {todo_to_remove} was removed from the list."
+            print(message)
+        except IndexError:
+            print("There is no item with that number.")
+            continue
+
+    elif user_action.startswith('exit'):
+        break
+    else:
+        print("Command is not valid")
+print ("Bye!")
