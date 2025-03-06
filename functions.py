94978d8ab32edcfc6601cc6a020f4158f4221b2f
oluwafisayo <oluwafisayotheophilus@gmail.com>
2025-03-06 16:21:35 -0500

    Create basic GUI without interactions, day 16

--- functions.py
+++ functions.py
@@ -13,10 +13,10 @@ def write_todos(todos_arg, filepath=FILEPATH):
         file.writelines(todos_arg)
 
 #this will not allow the print statement to work when called
-#in main.py
+#in cli.py
 #if we run name which is variable directly in the same py file.
 # the output will be main. but if we run functions.py which
-# contain the __name__ in the main.py the output will be functions.
+# contain the __name__ in the cli.py the output will be functions.
 # to control the execution when the file is run directly
 if __name__ == "__main__":
     print("Hello")
